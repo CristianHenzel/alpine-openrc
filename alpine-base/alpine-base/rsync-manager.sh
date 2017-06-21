94a0bcd3e9e885f87c58afd79f7c11f87f2d775a
Cristian Henzel <CristianHenzel@users.noreply.github.com>
2017-06-21 10:04:29 +0300

    Update rsync-manager.sh

--- alpine-base/alpine-base/rsync-manager.sh
+++ alpine-base/alpine-base/rsync-manager.sh
@@ -2,23 +2,20 @@
 
 PIDFILE=/var/run/rsync-manager.pid
 
-_pidenv() {
-	xargs -n 1 -0 < /proc/${1:-self}/environ | grep "^$2=" | cut -d= -f2
+_pid1env() {
+	xargs -n 1 -0 < /proc/1/environ | grep "^$2=" | cut -d= -f2
 }
 
-RSYNC_SERVER=$(_pidenv 1 RSYNC_SERVER)
-RSYNC_FOLDERS=$(_pidenv 1 RSYNC_FOLDERS)
-RSYNC_STIME=$(_pidenv 1 RSYNC_STIME)
+RSYNC_SERVER=$(_pid1env RSYNC_SERVER)
+RSYNC_FOLDERS=$(_pid1env RSYNC_FOLDERS)
+RSYNC_STIME=$(_pid1env RSYNC_STIME)
 
 if [ "$1" = "client" ]; then
 	# Running in client mode
 	while true; do
 		for dir in ${RSYNC_FOLDERS//,/}; do
-			RSYNC_OUTPUT=$(rsync -aEim rsync://rsync-manager/rsync/meta/$dir /rsync/meta/)
-			if [ $? -eq 0 ]; then
-				if [ -n "${RSYNC_OUTPUT}" ]; then
-					rsync -aEim rsync://rsync-manager/rsync/data/$dir /rsync/data/
-				fi
+			if [ -n "${RSYNC_OUTPUT}" ]; then
+				rsync -aEim rsync://rsync-manager/rsync/data/$dir /rsync/data/
 			fi
 			sleep ${RSYNC_STIME}
 		done
