1863463347de720b1e46be3d8531e064e08a9b6b
Cristian Henzel <CristianHenzel@users.noreply.github.com>
2017-06-21 10:02:50 +0300

    Update rsync-manager.sh

--- alpine-base/alpine-base/rsync-manager.sh
+++ alpine-base/alpine-base/rsync-manager.sh
@@ -1,32 +1,14 @@
 #!/bin/sh
 
-INOTIFY_EVENTS="-e modify -e attrib -e close -e create -e move -e delete"
 PIDFILE=/var/run/rsync-manager.pid
 
 _pidenv() {
 	xargs -n 1 -0 < /proc/${1:-self}/environ | grep "^$2=" | cut -d= -f2
 }
 
-_createhashes() {
-	for dir in /rsync/data/*/; do
-		dirname=$(basename $dir)
-		tar cP $dir | md5sum | awk '{print $1}' > /tmp/$dirname-meta
-		mv /tmp/$dirname-meta /rsync/meta/$dirname
-	done
-}
-
 RSYNC_SERVER=$(_pidenv 1 RSYNC_SERVER)
 RSYNC_FOLDERS=$(_pidenv 1 RSYNC_FOLDERS)
 RSYNC_STIME=$(_pidenv 1 RSYNC_STIME)
-
-if [ "$1" = "server" ]; then
-	# Running in server mode
-	_createhashes
-
-	while inotifywait ${INOTIFY_EVENTS} /rsync/data; do
-		_createhashes
-	done
-fi
 
 if [ "$1" = "client" ]; then
 	# Running in client mode
@@ -44,14 +26,10 @@ if [ "$1" = "client" ]; then
 fi
 
 if [ "$1" = "start" ]; then
-	if [ "${RSYNC_SERVER}" = "true" ]; then
-		RSYNC_MANAGER_RUNTYPE=server
-	else
-		RSYNC_MANAGER_RUNTYPE=client
+	if [ "${RSYNC_SERVER}" != "true" ]; then
+		setsid /var/alpine-base/rsync-manager.sh client >/dev/null 2>&1 < /dev/null &
+		echo $! > ${PIDFILE}
 	fi
-
-	setsid /var/alpine-base/rsync-manager.sh ${RSYNC_MANAGER_RUNTYPE} >/dev/null 2>&1 < /dev/null &
-	echo $! > ${PIDFILE}
 fi
 
 exit
