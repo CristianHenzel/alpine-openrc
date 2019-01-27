7c13f1f0e4d41031eaf5094b9ba6cee9a54285b7
Cristian Henzel <CristianHenzel@users.noreply.github.com>
2019-01-27 12:20:39 +0200

    Create config.sh

---
+++ make-vm-image/config.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+_step_counter=0
+step() {
+	_step_counter=$(( _step_counter + 1 ))
+	printf '\n\033[1;36m%d) %s\033[0m\n' $_step_counter "$@" >&2  # bold cyan
+}
+
+step "Installing dependencies"
+apt update && apt install libguestfs-tools linux-image-generic
+
