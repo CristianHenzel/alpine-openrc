7fb9f1f52091bece73574366ec74d3f50850ce62
Cristian Henzel <CristianHenzel@users.noreply.github.com>
2019-01-27 14:10:53 +0200

    Update config.sh

--- make-vm-image/config.sh
+++ make-vm-image/config.sh
@@ -8,12 +8,13 @@ step() {
 	printf '\n\033[1;36m%d) %s\033[0m\n' $_step_counter "$@" >&2  # bold cyan
 }
 
+rm -rf /download/*
+mkdir /download/root
+
 step "Installing dependencies"
 apt-get update && apt-get -y install debootstrap libguestfs-tools linux-image-amd64
 
 step "Installing base OS"
-rm -rf /download/*
-mkdir /download/root
 debootstrap testing /download/root http://deb.debian.org/debian/
 
 step "Done"
