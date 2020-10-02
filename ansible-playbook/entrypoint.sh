c1c7273ca24c06892e7ae1e151eeeb0356d6a4f1
Cristian Henzel <CristianHenzel@users.noreply.github.com>
2020-10-02 16:46:07 -0700

    Update entrypoint.sh

--- ansible-playbook/entrypoint.sh
+++ ansible-playbook/entrypoint.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-git clone "${PLAYBOOK_REPO}" /ansible/playbooks && \
+set -ex
+
+git clone "${PLAYBOOK_REPO}" /ansible/playbooks
 ansible-playbook -e "ansible_user=${ANSIBLE_USER} ansible_pass=${ANSIBLE_PASS}" playbook.yml
